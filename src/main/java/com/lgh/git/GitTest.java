084ab4fdfd6c45e5912013adb1d285b9d10d080d
lgh <2385998151@qq.com>
2022-09-09 21:38:42 +0800

    second commit

--- src/main/java/com/lgh/git/GitTest.java
+++ src/main/java/com/lgh/git/GitTest.java
@@ -3,5 +3,6 @@ package com.lgh.git;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello git!");
+        System.out.println("hello java!");
     }
 }
