801d7e77cd847f2e2576ac690731a0b373632cdc
lgh520LGH <93120326+lgh520LGH@users.noreply.github.com>
2022-09-09 22:30:07 +0800

    pull commit

--- src/main/java/com/lgh/git/GitTest.java
+++ src/main/java/com/lgh/git/GitTest.java
@@ -9,5 +9,6 @@ public class GitTest {
         System.out.println("master test!");
         System.out.println("hot-fix commit!");
         System.out.println("PUSH TEST!");
+        System.out.println("pull test!");
     }
 }
