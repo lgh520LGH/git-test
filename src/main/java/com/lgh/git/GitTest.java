169b040affe64cf129c9fb1283320f09dc4118a4
lgh <2385998151@qq.com>
2022-09-09 21:46:46 +0800

    hot-fix commit

--- src/main/java/com/lgh/git/GitTest.java
+++ src/main/java/com/lgh/git/GitTest.java
@@ -5,5 +5,6 @@ public class GitTest {
         System.out.println("hello git!");
         System.out.println("hello java!");
         System.out.println("hello world!");
+        System.out.println("hello idea!");
     }
 }
